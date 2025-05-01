b3004a79ef5b2f0576c48ae73a890790ba7bfc56
Malathi Dasari <malathidasari30@gmail.com>
2025-05-01 15:58:57 +0530

    c

--- src/common/SummaryApi.js
+++ src/common/SummaryApi.js
@@ -1,9 +1,10 @@
 export const baseURL = "https://blinkitserver-pi.vercel.app";
+// https://blinkitserver-pi.vercel.app/
 
 const SummaryApi = {
   register: {
     url: "/api/user/register",
-    method: "post",
+    method: "post",cd 
   },
   login: {
     url: "/api/user/login",
@@ -14,7 +15,7 @@ const SummaryApi = {
     method: "put",
   },
   forgot_password_otp_verification: {
-    url: "api/user/verify-forgot-password-otp",
+    url: "/api/user/verify-forgot-password-otp",
     method: "put",
   },
   resetPassword: {
@@ -22,7 +23,7 @@ const SummaryApi = {
     method: "put",
   },
   refreshToken: {
-    url: "api/user/refresh-token",
+    url: "/api/user/refresh-token",
     method: "post",
   },
   userDetails: {
